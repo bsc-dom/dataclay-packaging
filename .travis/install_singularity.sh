53a7781075b729709db1f367b6972eb9ea45c412
dgasull <dgasull@bsc.es>
2020-04-07 18:32:57 +0200

    Using gimme for installing go

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -11,24 +11,23 @@ sudo apt-get update && sudo apt-get install -y \
     git \
     cryptsetup
 
-sudo rm -rf /usr/local/go   
-export VERSION=1.13 OS=linux ARCH=amd64
-wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz 
-sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
+#sudo rm -rf /usr/local/go   
+#export VERSION=1.13 OS=linux ARCH=amd64
+#wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz 
+#sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+#rm go$VERSION.$OS-$ARCH.tar.gz
 
-echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc 
-source ~/.bashrc
+#echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc 
+#source ~/.bashrc
+eval "$(gimme $TRAVIS_GO_VERSION)"
 
 export VERSION=3.5.2
 wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
 tar -xzf singularity-${VERSION}.tar.gz 
 pushd singularity
 
-export PATH=/usr/local/go/bin:$PATH
+#export PATH=/usr/local/go/bin:$PATH
 go version
-which go
-ls /usr/local/go/bin 
     
 ./mconfig && \
     make -C builddir && \
