2046b23f36154710846b1bdc258bc969c7e3001f
dgasull <dgasull@bsc.es>
2020-04-07 18:22:01 +0200

    Adding singularity cloud token

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -11,6 +11,18 @@ sudo apt-get update && sudo apt-get install -y \
     git \
     cryptsetup
 
+sudo rm -rf /usr/local/go   
+export VERSION=1.13 OS=linux ARCH=amd64 && \
+   wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
+   sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
+   rm go$VERSION.$OS-$ARCH.tar.gz
+
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc 
+source ~/.bashrc
+go version
+which go
+ls /usr/local/go/bin 
+
 export VERSION=3.5.2 && \
     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
     tar -xzf singularity-${VERSION}.tar.gz && \
