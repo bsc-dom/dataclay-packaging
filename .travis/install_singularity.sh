10e93ac4eb453ae33c108e901432429684e9e66a
dgasull <dgasull@bsc.es>
2020-04-07 18:25:56 +0200

    Modified install singularity script

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -12,22 +12,25 @@ sudo apt-get update && sudo apt-get install -y \
     cryptsetup
 
 sudo rm -rf /usr/local/go   
-export VERSION=1.13 OS=linux ARCH=amd64 && \
-   wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-   sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
-   rm go$VERSION.$OS-$ARCH.tar.gz
+export VERSION=1.13 OS=linux ARCH=amd64
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz 
+sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+rm go$VERSION.$OS-$ARCH.tar.gz
 
 echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc 
 source ~/.bashrc
+
+export VERSION=3.5.2
+wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+tar -xzf singularity-${VERSION}.tar.gz 
+pushd singularity
+
 go version
 which go
 ls /usr/local/go/bin 
-
-export VERSION=3.5.2 && \
-    wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
-    tar -xzf singularity-${VERSION}.tar.gz && \
-    cd singularity
     
 ./mconfig && \
     make -C builddir && \
     sudo make -C builddir install
+
+popd
