b1369b00fe0c4cc55652adc6bb2254a8ecab077c
dgasull <dgasull@bsc.es>
2020-04-07 18:09:42 +0200

    Making packaging a go project

--- .travis/install_singularity.sh
+++ .travis/install_singularity.sh
@@ -11,15 +11,6 @@ sudo apt-get update && sudo apt-get install -y \
     git \
     cryptsetup
 
-sudo rm -rf /usr/local/go   
-export VERSION=1.13 OS=linux ARCH=amd64 && \
-   wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz && \
-   sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz && \
-   rm go$VERSION.$OS-$ARCH.tar.gz
-
-echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.bashrc 
-source ~/.bashrc
-
 export VERSION=3.5.2 && \
     wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
     tar -xzf singularity-${VERSION}.tar.gz && \
