898a9558b7b27f0175bf4a5fd5b068d8999ddb6f
dgasull <dgasull@bsc.es>
2020-04-24 17:22:33 +0200

    Replacing tag in module

--- .travis/marenostrum/deploy.sh
+++ .travis/marenostrum/deploy.sh
@@ -40,7 +40,7 @@ singularity pull $LOCAL_REPOSITORY/dspython.sif library://support-dataclay/defau
 singularity pull $LOCAL_REPOSITORY/client.sif library://support-dataclay/default/client:$DEFAULT_TAG
 
 # Prepare module definition 
-sed 's/SET_VERSION_HERE/${DEFAULT_TAG}/g' ./.travis/marenostrum/module.lua > /tmp/${DEFAULT_TAG}.lua
+sed "s/SET_VERSION_HERE/${DEFAULT_TAG}/g" ./.travis/marenostrum/module.lua > /tmp/${DEFAULT_TAG}.lua
 
 # Deploy singularity and orchestration scripts to Marenostrum
 ssh dataclay@mn1.bsc.es "rm -rf /apps/DATACLAY/$DEFAULT_TAG/ && mkdir -p /apps/DATACLAY/$DEFAULT_TAG/singularity/images/" #sanity check
