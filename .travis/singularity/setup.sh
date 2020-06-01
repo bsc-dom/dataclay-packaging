974f3d61e2fe98f86383ee3137ea8661a1e01cbe
dgasull <dgasull@bsc.es>
2020-06-01 18:57:54 +0200

    Removed singularity token

--- .travis/singularity/setup.sh
+++ .travis/singularity/setup.sh
@@ -24,13 +24,3 @@ pushd singularity
     sudo make -C builddir install
 
 popd
-
-# Login singularity
-openssl aes-256-cbc \
-	-K $encrypted_0680b2354a01_key \
-	-iv $encrypted_0680b2354a01_iv \
-	-in .travis/singularity/singularity_cloud_token.enc \
-	-out .travis/singularity/singularity_cloud_token -d
-	
-singularity remote login < .travis/singularity/singularity_cloud_token
-
