479234dbe218068017586d6ece3f99fbe0460c55
dgasull <dgasull@bsc.es>
2021-01-08 22:46:36 +0100

    Modified MN secrets

--- docker/continous-integration/packaging-mn/entrypoint.sh
+++ docker/continous-integration/packaging-mn/entrypoint.sh
@@ -4,10 +4,9 @@
 git submodule init
 git submodule update
 
-# Login in Dockerhub
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
 # Install  keys
+echo $MN_SALT
+echo $MN_SECRET
 /appveyor-tools/secure-file -decrypt .appveyor/mn_deploy_key.enc -secret $MN_SECRET -salt $MN_SALT
 mv .appveyor/mn_deploy_key $HOME/.ssh/mn_deploy_key
 
